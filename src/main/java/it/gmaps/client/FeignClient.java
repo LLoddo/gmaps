daf0debcebbd857743867a5839f0a8133a158e0a
Luca Loddo <l.loddo@campus.unimib.it>
2023-03-16 18:02:23 +0100

    [Development] key as query param to feign client added + annotation dependency to pom added

--- src/main/java/it/gmaps/client/FeignClient.java
+++ src/main/java/it/gmaps/client/FeignClient.java
@@ -4,11 +4,12 @@ import it.gmaps.request.AddressRequest;
 import it.gmaps.response.AddressResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.cloud.openfeign.FeignClient("https://addressvalidation.googleapis.com/v1:")
 public interface FeignClient {
 
     @PostMapping(value = "validateAddress")
-    AddressResponse postValidateAddress(@RequestBody AddressRequest addressRequest);
+    AddressResponse postValidateAddress(@RequestBody AddressRequest addressRequest, @RequestParam String key);
 
 }
